827be9d35522698ea2654d7f98b279bb9d0b679e
Ashutosh <gsasha220@gmail.com>
2022-11-05 22:54:08 +0530

    Initial commit

---
+++ deploy.js
@@ -0,0 +1,7 @@
+function main(){
+    console.log('hi'); //to run this using node , go to terminal and type "node deploy.js" that is name of file and press enter this way it will run js code in backend
+    let variable=8;
+    console.log(variable);
+    }
+    main();
+    
